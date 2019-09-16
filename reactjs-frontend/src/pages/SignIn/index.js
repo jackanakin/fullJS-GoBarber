16666fd24e9a16053dac3651ab40dfb1da6eb07f
Jardel Kuhn <jardelkuhn@gmail.com>
2019-09-16 02:17:49 -0300

    chore: added auth

--- reactjs-frontend/src/pages/SignIn/index.js
+++ reactjs-frontend/src/pages/SignIn/index.js
@@ -1,21 +1,34 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import { signInRequest } from '~/store/modules/auth/actions';
 
 import logo from '~/assets/logo.svg';
 
-// import { Container } from './styles';
+const schema = Yup.object().shape({
+    email: Yup.string()
+        .email('Insira um e-mail válido')
+        .required('O e-mail é obrigatório'),
+    password: Yup.string()
+        .min(6, 'Mínimo de 6 caracteres')
+        .required('A senha é obrigatória'),
+});
 
 export default function SignIn() {
-    function handleSubmit(data) {
-        console.log(data)
+    const dispatch = useDispatch();
+
+    function handleSubmit({ email, senha }) {
+        dispatch(signInRequest(email, senha));
     }
 
     return (
         <>
             <img src={logo} alt="GoBarber" />
 
-            <Form onSubmit={handleSubmit}>
+            <Form schema={schema} onSubmit={handleSubmit}>
                 <Input type="email" name="email" placeholder="Seu e-mail" />
                 <Input
                     type="passowrd"
